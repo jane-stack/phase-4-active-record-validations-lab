24cf3f9c09bf412141fefe6a663e33ccf2eb28ae
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-02-10 12:21:46 -0700

    commit tree 5f4be05ade631c70a2afd5f1dc7c2f821bade0d8

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    
+    validate :is_clickbait
+
+    def is_clickbait
+        if title.present? && title.exclude?("Won't Believe" || "Secret" || "Top" || "Guess")
+            errors.add(:title, "Needs more clickbait!")
+        end
+    end
+
 end
